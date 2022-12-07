cf184891c6d46cd68a970e96bfb2d42711ec62de
Serhii Charykov <laammaar@gmail.com>
2022-12-07 02:08:34 +0200

    Clang Format

--- bencode_fast/bencodemodule.c
+++ bencode_fast/bencodemodule.c
@@ -4,131 +4,127 @@
 #define UNUSED(x) (void)(x)
 static const int BUFF_SIZE = 20;
 
-static PyObject *
-decode_string(const char *bytes, Py_ssize_t size, Py_ssize_t *remaining_size) {
-    const char *pos = bytes;  // Redundant?
-    Py_ssize_t used_size = 0;
-    PyObject *plength = NULL, *result = NULL;
-    char buff[BUFF_SIZE];
-    
-    // Find smth like strcpy_s
-    while (used_size < size && isdigit(*pos)) {
-        if (used_size >= BUFF_SIZE - 1) {
-            PyErr_SetString(PyExc_ValueError, "String is too long");
-            goto error;
-        }
-        buff[used_size++] = *pos++;
-    }
-    buff[used_size] = 0;
+static PyObject *decode_string(const char *bytes, Py_ssize_t size,
+                               Py_ssize_t *remaining_size) {
+  const char *pos = bytes; // Redundant?
+  Py_ssize_t used_size = 0;
+  PyObject *plength = NULL, *result = NULL;
+  char buff[BUFF_SIZE];
 
-    plength = PyLong_FromString(buff, NULL, 10);
-    if (plength == NULL) {
-        goto error;
+  // Find smth like strcpy_s
+  while (used_size < size && isdigit(*pos)) {
+    if (used_size >= BUFF_SIZE - 1) {
+      PyErr_SetString(PyExc_ValueError, "String is too long");
+      goto error;
     }
-    long length = PyLong_AsLong(plength);
-    if (length == -1 && PyErr_Occurred() != NULL) {
-        goto error;
-    }
+    buff[used_size++] = *pos++;
+  }
+  buff[used_size] = 0;
 
-    if (used_size++ > size || *pos++ != ':') {
-        PyErr_SetString(PyExc_ValueError, "Missing \":\" after string length");
-        goto error;
-    }
-    if (used_size + length > size) {
-        PyErr_SetString(PyExc_ValueError, "Missing required number of bytes after \":\"");
-        goto error;
-    }
-    result = PyUnicode_Decode(pos, length, "utf-8", "strict");
-    used_size += length;
-    if (remaining_size != NULL) {
-        *remaining_size = size - used_size;
-    }
+  plength = PyLong_FromString(buff, NULL, 10);
+  if (plength == NULL) {
+    goto error;
+  }
+  long length = PyLong_AsLong(plength);
+  if (length == -1 && PyErr_Occurred() != NULL) {
+    goto error;
+  }
+
+  if (used_size++ > size || *pos++ != ':') {
+    PyErr_SetString(PyExc_ValueError, "Missing \":\" after string length");
+    goto error;
+  }
+  if (used_size + length > size) {
+    PyErr_SetString(PyExc_ValueError,
+                    "Missing required number of bytes after \":\"");
+    goto error;
+  }
+  result = PyUnicode_Decode(pos, length, "utf-8", "strict");
+  used_size += length;
+  if (remaining_size != NULL) {
+    *remaining_size = size - used_size;
+  }
 
 error:
 
-    Py_XDECREF(plength);
-    return result;
+  Py_XDECREF(plength);
+  return result;
 }
 
+static PyObject *decode_integer(const char *bytes, Py_ssize_t size,
+                                Py_ssize_t *remaining_size) {
+  const char *pos = bytes; // Redundant?
+  Py_ssize_t used_size = 0, buff_index = 0;
+  PyObject *result = NULL;
+  char buff[BUFF_SIZE];
 
-static PyObject *
-decode_integer(const char *bytes, Py_ssize_t size, Py_ssize_t *remaining_size) {
-    const char *pos = bytes;  // Redundant?
-    Py_ssize_t used_size = 0, buff_index = 0;
-    PyObject *result = NULL;
-    char buff[BUFF_SIZE];
-    
-    pos = bytes;
-    // Check for 'i'
-    if (used_size++ > size || *pos++ != 'i') {
-        PyErr_SetString(PyExc_ValueError, "Missing \"i\" before integer");
-        goto error;
-    }
+  pos = bytes;
+  // Check for 'i'
+  if (used_size++ > size || *pos++ != 'i') {
+    PyErr_SetString(PyExc_ValueError, "Missing \"i\" before integer");
+    goto error;
+  }
 
-    // Optional '-'
-    if (used_size < size && *pos == '-') {
-        buff[buff_index++] = *pos++;
-        used_size++;
-    }
+  // Optional '-'
+  if (used_size < size && *pos == '-') {
+    buff[buff_index++] = *pos++;
+    used_size++;
+  }
 
-    // TODO: Same as above
-    // Copy digits to \0 terminated string
-    while (used_size < size && isdigit(*pos)) {
-        if (buff_index >= BUFF_SIZE - 1) {
-            PyErr_SetString(PyExc_ValueError, "Integer is too long");
-            goto error;
-        }
-        buff[buff_index++] = *pos++;
-        used_size++;
+  // TODO: Same as above
+  // Copy digits to \0 terminated string
+  while (used_size < size && isdigit(*pos)) {
+    if (buff_index >= BUFF_SIZE - 1) {
+      PyErr_SetString(PyExc_ValueError, "Integer is too long");
+      goto error;
     }
-    buff[buff_index] = 0;
+    buff[buff_index++] = *pos++;
+    used_size++;
+  }
+  buff[buff_index] = 0;
 
-    // Convert to Python int
-    result = PyLong_FromString(buff, NULL, 10);
-    if (result == NULL) {
-        goto error;
-    }
-    
-    // Check for 'e'
-    if (used_size++ > size || *pos++ != 'e') {
-        PyErr_SetString(PyExc_ValueError, "Missing \"e\" after integer");
-        goto error;
-    }
+  // Convert to Python int
+  result = PyLong_FromString(buff, NULL, 10);
+  if (result == NULL) {
+    goto error;
+  }
 
-    if (remaining_size != NULL) {
-        *remaining_size = size - used_size;
-    }
+  // Check for 'e'
+  if (used_size++ > size || *pos++ != 'e') {
+    PyErr_SetString(PyExc_ValueError, "Missing \"e\" after integer");
+    goto error;
+  }
 
-    return result;
+  if (remaining_size != NULL) {
+    *remaining_size = size - used_size;
+  }
+
+  return result;
 error:
 
-    Py_CLEAR(result);
-    return result;
+  Py_CLEAR(result);
+  return result;
 }
 
-static PyObject *
-decode(PyObject *self, PyObject *input_bytes) {
-    UNUSED(self);
-    const char *bytes;
+static PyObject *decode(PyObject *self, PyObject *input_bytes) {
+  UNUSED(self);
+  const char *bytes;
 
-    bytes = PyBytes_AsString(input_bytes);
-    if (bytes == NULL) {
-        return NULL;
-    }
-    Py_ssize_t size = PyBytes_Size(input_bytes);
+  bytes = PyBytes_AsString(input_bytes);
+  if (bytes == NULL) {
+    return NULL;
+  }
+  Py_ssize_t size = PyBytes_Size(input_bytes);
 
-    // return decode_string(bytes, size, NULL);
-    Py_ssize_t tmp = size;
-    PyObject *result = decode_integer(bytes, size, &tmp);
-    fprintf(stderr, "\nprevios size: %zu, remaining size: %zu", size, tmp);
-    return result;
+  // return decode_string(bytes, size, NULL);
+  Py_ssize_t tmp = size;
+  PyObject *result = decode_integer(bytes, size, &tmp);
+  fprintf(stderr, "\nprevios size: %zu, remaining size: %zu", size, tmp);
+  return result;
 }
-
 
 static PyMethodDef DecodeMethods[] = {
-    {"decode", decode, METH_O, "Decode bencode"},
-    {NULL, NULL, 0, NULL}
-};
+    {"decode", decode, METH_O, "Decode bencode"}, {NULL, NULL, 0, NULL}};
 
 static struct PyModuleDef bencodemodule = {
     PyModuleDef_HEAD_INIT,
@@ -142,9 +138,6 @@ static struct PyModuleDef bencodemodule = {
     NULL,
 };
 
-
-PyMODINIT_FUNC
-PyInit_bencodemodule(void)
-{
-   return PyModule_Create(&bencodemodule);
+PyMODINIT_FUNC PyInit_bencodemodule(void) {
+  return PyModule_Create(&bencodemodule);
 }
